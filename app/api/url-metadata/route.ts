20742e8e3d4b94625e258af9ebec7ee21fa9d1d0
Berteotti <berteotti2@gmail.com>
2024-07-25 17:10:52 +0100

    fix

--- app/api/url-metadata/route.ts
+++ app/api/url-metadata/route.ts
@@ -117,6 +117,6 @@ export async function GET(request: NextRequest) {
   } catch (error) {
     console.log(error);
 
-    return NextResponse.json({ error: "something wrong" }, { status: 400 });
+    return NextResponse.json({ error: error }, { status: 400 });
   }
 }
