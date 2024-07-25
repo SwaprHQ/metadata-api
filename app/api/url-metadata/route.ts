7867a1cbc19395e127aaa00c135f849867a47a0c
Berteotti <berteotti2@gmail.com>
2024-07-25 17:06:30 +0100

    migration to nextjs vanilla

--- app/api/url-metadata/route.ts
+++ app/api/url-metadata/route.ts
@@ -1,7 +1,7 @@
-import { NextResponse } from 'next/server';
-import puppeteer from 'puppeteer-core';
-const cheerio = require('cheerio');
-const chrome = require('chrome-aws-lambda');
+import { NextResponse, NextRequest } from "next/server";
+import puppeteer from "puppeteer-core";
+const cheerio = require("cheerio");
+const chrome = require("chrome-aws-lambda");
 
 /** The code below determines the executable location for Chrome to
  * start up and take the screenshot when running a local development environment.
@@ -15,16 +15,23 @@ const chrome = require('chrome-aws-lambda');
  via https://www.contentful.com/blog/2021/03/17/puppeteer-node-open-graph-screenshot-for-socials/
 */
 
+export const dynamicParams = true;
+export const revalidate = false;
+export const fetchCache = "auto";
+export const runtime = "nodejs";
+export const preferredRegion = "auto";
+export const dynamic = "force-dynamic";
+
 const exePath =
-  process.platform === 'win32'
-    ? 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe'
-    : process.platform === 'linux'
-      ? '/usr/bin/google-chrome'
-      : '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome';
+  process.platform === "win32"
+    ? "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
+    : process.platform === "linux"
+    ? "/usr/bin/google-chrome"
+    : "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome";
 
 const getOptions = async () => {
   let options;
-  if (process.env.NODE_ENV === 'production') {
+  if (process.env.NODE_ENV === "production") {
     options = {
       args: chrome.args,
       executablePath: await chrome.executablePath,
@@ -40,71 +47,76 @@ const getOptions = async () => {
   return options;
 };
 
-export async function GET(request: Request) {
+export async function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams;
+
+  const urlParam = searchParams.get("url");
   try {
     console.log(request.url);
-    const url = new URL(request.url);
-
-    const urlParam = url.searchParams.get('url');
     console.log(urlParam);
-    if (!urlParam) throw null;
+    if (!urlParam) throw "No url";
 
     const decodedUrl = decodeURIComponent(urlParam);
 
-    console.log('configuring chrome...');
+    console.log("configuring chrome...");
     const options = await getOptions();
 
-    console.log('launching browser...');
+    console.log("launching browser...");
     const browser = await puppeteer.launch(options);
 
-    console.log('opening new page...');
+    console.log("opening new page...");
     const page = await browser.newPage();
 
-    console.log('setting request interception...');
+    console.log("setting request interception...");
     await page.setRequestInterception(true);
-    page.on('request', (request: any) => {
+    page.on("request", (request: any) => {
       const reqType = request.resourceType();
-      if (reqType === 'document') {
+      if (reqType === "document") {
         request.continue();
-      } else if (process.env.NODE_ENV === 'development') {
+      } else if (process.env.NODE_ENV === "development") {
         request.continue();
       } else {
-        console.log('block request type: ' + request.resourceType());
+        console.log("block request type: " + request.resourceType());
         request.abort();
       }
     });
 
-    console.log('navigating to ' + decodedUrl + '...');
+    console.log("navigating to " + decodedUrl + "...");
     await page.goto(decodedUrl, { timeout: 0 }).then(() => {
-      console.log('url loaded'); //WORKS FINE
+      console.log("url loaded"); //WORKS FINE
     });
 
-    console.log('get page content...');
+    console.log("get page content...");
 
     const html =
-      process.env.NODE_ENV === 'development'
+      process.env.NODE_ENV === "development"
         ? await page.content()
         : await page.evaluate(() => {
-            return document?.querySelector('body')?.innerHTML;
+            return document?.querySelector("body")?.innerHTML;
           });
 
-    console.log('parse html...');
+    console.log("parse html...");
     const $ = cheerio.load(html);
 
-    const articlePusblishTime = $('[property="article:published_time"]').attr('content');
-    const ogImage = $('[property="og:image"]').attr('content');
-    const ogTitle = $('[property="og:title"]').attr('content');
+    const articlePusblishTime = $('[property="article:published_time"]').attr(
+      "content"
+    );
+    const ogImage = $('[property="og:image"]').attr("content");
+    const ogTitle = $('[property="og:title"]').attr("content");
 
     console.log({ articlePusblishTime, ogImage, ogTitle });
 
-    console.log('closing browser...');
+    console.log("closing browser...");
     await browser.close();
 
-    console.log('done.');
-    return NextResponse.json({ articlePusblishTime, ogImage, ogTitle }, { status: 201 });
+    console.log("done.");
+    return NextResponse.json(
+      { articlePusblishTime, ogImage, ogTitle },
+      { status: 201 }
+    );
   } catch (error) {
     console.log(error);
 
-    return NextResponse.json({ error: error }, { status: 400 });
+    return NextResponse.json({ error: "something wrong" }, { status: 400 });
   }
 }
--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,8 @@
-import { setupDevPlatform } from "@cloudflare/next-on-pages/next-dev";
-
-// Here we use the @cloudflare/next-on-pages next-dev module to allow us to use bindings during local development
-// (when running the application with `next dev`), for more information see:
-// https://github.com/cloudflare/next-on-pages/blob/main/internal-packages/next-dev/README.md
-if (process.env.NODE_ENV === "development") {
-  await setupDevPlatform();
-}
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack(config) {
-    config.externals = ["chrome-aws-lambda", "puppeteer-core"];
+  webpack: (config, { webpack }) => {
+    //config.externals = ["chrome-aws-lambda", "puppeteer-core"];
+    config.externals = ["chrome-aws-lambda", "puppeteer-core", "picocolors"];
 
     return config;
   },
